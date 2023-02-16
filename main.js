c69348737937eb607239879c4f7ca56409c64d5a
Ash2374 <110349011+Ash2374@users.noreply.github.com>
2023-02-15 20:07:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload()
+{
+
+}
+
+
+function setup()
+    {
+canvas=createCanvas(300,300);
+canvas.center();
+    }
+
+
+    function draw()
+    {
+
+    }
+
+    function t_snap()
+    {
+save('myimg.png');
+    }
+   
